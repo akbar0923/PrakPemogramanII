b34889be1653a9ad02b9ae648a72a84b3c935512
akbar0923 <72205800+akbar0923@users.noreply.github.com>
2022-11-16 14:55:32 +0800

    Add files via upload

---
+++ praktikum 4/soal1/Buku.java
@@ -0,0 +1,20 @@
+package praktikum4.soal1;
+
+public class Buku {
+    public String judul;
+    public String penulis;
+    public int tahun;
+    
+    public Buku(String j, String p, int t) {
+        this.judul = j;
+        this.penulis = p;
+        this.tahun = t;
+    }
+
+    public void display(){
+        System.out.println("Detail Buku :");
+        System.out.println("Judul adalah " + judul);
+        System.out.println("penulis adalah " + penulis);
+        System.out.println("Tahun Terbit adalah " + tahun);
+    }
+}
